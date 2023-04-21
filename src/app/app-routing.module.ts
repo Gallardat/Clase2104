569f7cda6ed96faeb7d63f866035f0f0c35a256f
daniela.gallardo@softtek.com <daniela.gallardo@softtek.com>
2023-04-21 14:07:07 +0200

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BlogComponent } from './blog/blog.component';
+import { Articulo1Component } from './articulo1/articulo1.component';
+import { Articulo2Component } from './articulo2/articulo2.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:InicioComponent},
+  {path:"habilidades", component:HabilidadesComponent},
+  {path:"contacto",component:ContactoComponent},
+  {path:"blog",component:BlogComponent},
+  {path:'articulo1',component:Articulo1Component},
+  {path:'articulo2', component:Articulo2Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BlogComponent } from './blog/blog.component';
+import { Articulo1Component } from './articulo1/articulo1.component';
+import { Articulo2Component } from './articulo2/articulo2.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    BlogComponent,
+    Articulo1Component,
+    Articulo2Component,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
